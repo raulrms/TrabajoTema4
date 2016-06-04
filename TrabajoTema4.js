87bcc4058bd32b66c0ad071dc573186de5052747
Raúl Martínez <raulrms@yahoo.es>
2016-06-04 16:23:30 +0200

    Update TrabajoTema4.js

--- TrabajoTema4.js
+++ TrabajoTema4.js
@@ -1,6 +1,12 @@
-//MongoBooster support fluent query builder API.
-//instead of writing:
-//db.user.find({age:{$gte:21,$lte:65}},{name,1,age:1,_id:-1}).sort({age:-1, name:1});
-//we can instead write:
-//db.user.where('age').gte(21).lte(65).select('name age -_id').sort("-age name");    
-db.libros.find({})
+//1. Crea cada uno de los diez documentos en una collection llamada libros en la base de datos biblioteca; 
+db.libros.insert({"titulo":"MongoDB: The definitive guide","autores":[{"nombre":"Kristina","apellidos":"Chodorow"},{"nombre":"Michael","apellidos":"Dirolf"}],"anio":2010,"prestamo":false})
+db.libros.insert({"titulo":"MongoDB in Action","autores":[{"nombre":"Kyle","apellidos":"Banker"}],"anio":2012,"prestamo":false})
+db.libros.insert({"titulo":"Professional NoSQL","autores":[{"nombre":"Shashank","apellidos":"Tiwari"}],"anio":2011,"prestamo":true})
+db.libros.insert({"titulo":"MongoDB Applied Design Patterns","autores":[{"nombre":"Rick","apellidos":"Copeland"}],"anio":2013,"prestamo":true})
+db.libros.insert({"titulo":"NoSQL Distilled","autores":[{"nombre":"Pramod J.","apellidos":"Sadalage"},{"nombre":"Martin","apellidos":"Fowler"}],"anio":2013,"prestamo":false})
+db.libros.insert({"titulo":"Algorithms","autores":[{"nombre":"Robert","apellidos":"Sedgewick"}],"anio":1983,"prestamo":true})
+db.libros.insert({"titulo":"Algorithms in a Nutshell","autores":[{"nombre":"George","apellidos":"Heineman"}],"anio":2008,"prestamo":true})
+db.libros.insert({"titulo":"Introduction to Algorithms","autores":[{"nombre":"Thomas H.","apellidos":"Cormen"}],"anio":1990,"prestamo":true})
+db.libros.insert({"titulo":"Algorithms Unlocked","autores":[{"nombre":"Thomas H.","apellidos":"Cormen"}],"anio":2013,"prestamo":false})
+db.libros.insert({"titulo":"Compilers: Principles, Techniques, and Tools","autores":[{"nombre":"Alfred V.","apellidos":"Aho"}],"anio":1986,"prestamo":true})
+
